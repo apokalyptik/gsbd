6eb6bb0c4807f16860d4fb938ded295e39505397
Demitrious Kelly <apokalyptik@apokalyptik.com>
2014-06-10 15:36:36 -0700

    first patch at batching

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ These are not automatically satisfied by "go get"
 ### TODO
 
 * websocket support
-* batching support
+* see if avoiding calling out to C gets us better concurrency in github.com/rjohnsondev/go-safe-browsing-api
 
 ### HTTP API
 
@@ -32,5 +32,13 @@ Whether the index is up to date or not. Returns "true" or "false"
 
 #### GET /safe/{URL}
 
-Checks the safe browsing database for {URL}.  Returns an empty string if the url is not listed, or a string representing the list that it was found in.
+Checks the safe browsing database for {URL}.  Returns an empty string 
+if the url is not listed, or a string representing the list that it 
+was found in.
 
+#### POST /batch
+
+Batch processing. Request body should be a json encoded array 
+of strings each of which is a url to check. Returns an array 
+of strings matching the requested strings indexes to the response.
+Just like /safe/{URL} it's "", or the list per response
--- http.go
+++ http.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -21,6 +22,47 @@ func webUpToDate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	}
 }
 
+func webSafeBatch(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var err error
+	var num = -1
+	var results []string
+	var req []string
+	var start = time.Now()
+	defer func() {
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			logger.Critical(fmt.Sprintf("batch=%d err=%s in=%f", num, err.Error(), time.Since(start).Seconds()))
+		} else {
+			if verbosity >= 2 {
+				logger.Info(fmt.Sprintf("batch=%d err=nil, in=%f", num, time.Since(start).Seconds()))
+			}
+		}
+		rsp, _ := json.Marshal(results)
+		fmt.Fprint(w, string(rsp))
+	}()
+	decoder := json.NewDecoder(r.Body)
+	if err = decoder.Decode(&req); err != nil {
+		return
+	}
+	num = len(req)
+	results = make([]string, num)
+	for k, url := range req {
+		results[k], _, err = gsb.MightBeListed(url)
+		if err != nil {
+			return
+		}
+
+		if results[k] == "" {
+			continue
+		}
+
+		results[k], err = gsb.IsListed(url)
+		if err != nil {
+			return
+		}
+	}
+}
+
 func webSafe(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	var result string
 	var err error
@@ -32,7 +74,9 @@ func webSafe(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 			w.WriteHeader(http.StatusInternalServerError)
 			logger.Critical(fmt.Sprintf("req=%s err=%s res=null in=%f", url, err.Error(), time.Since(start).Seconds()))
 		} else {
-			logger.Info(fmt.Sprintf("req=%s err=nil, res=%s in=%f", url, result, time.Since(start).Seconds()))
+			if verbosity >= 2 {
+				logger.Info(fmt.Sprintf("req=%s err=nil, res=%s in=%f", url, result, time.Since(start).Seconds()))
+			}
 		}
 		fmt.Fprint(w, result)
 	}()
@@ -49,6 +93,7 @@ func handleHTTP() {
 	router := httprouter.New()
 	router.GET("/", webIndex)
 	router.GET("/safe/*url", webSafe)
+	router.POST("/batch", webSafeBatch)
 	router.GET("/uptodate/", webUpToDate)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", httpHost, httpPort), router))
 }
